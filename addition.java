baf3d48ec08811ccc30b53ac395aa0e98fea6073
Nirav Sinha <niravsanjay@gmail.com>
2024-06-16 15:25:19 +0530

    First Commit

---
+++ addition.java
@@ -0,0 +1,12 @@
+public class addition
+{
+    public static void main(String[] args)
+    {
+    int a = 40;
+    int b = 60;
+    int c = a+b;
+
+    System.out.println("The addition of a and b is: " + c);
+
+    }
+} 
