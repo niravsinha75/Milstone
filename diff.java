c1c97714915c289933c92bdd20f4731f3aafc56f
Nirav Sinha <niravsanjay@gmail.com>
2024-06-16 16:19:42 +0530

    Second Commit

---
+++ diff.java
@@ -0,0 +1,14 @@
+public class diff {
+   
+    public static void main(String[] args)
+    {
+    int a = 100;
+    int b = 60;
+    int c = a-b;
+
+    System.out.println("The difference between a and b is: " + c);
+
+    }
+} 
+    
+
